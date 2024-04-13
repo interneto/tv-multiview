645efe138594405b6bb42afb6fe7472e250c6cad
Alplox <42575576+Alplox@users.noreply.github.com>
2024-04-13 12:37:06 -0400

    v0.17
    ## [v0.17]
    ### Changed
    - filtroCanales reescrito para tomar en cuenta si se pulsa filtro por país de modo de que el input usuario sea dentro de dicho país activo.
    - F_ordenBotones reescrito para abarcar modal dinámico de opción cambiar señal activa, guardando orden original de botones previo a ordenar de forma ascendente/descendente
    - div contenedor canales activos utiliza ahora el 100vh
    - Rediseño global
    - Imágenes manifesto PWA y Preview repositorio (gracias a https://shots.so/, https://pixlr.com/es/express/ y https://progressier.com/pwa-screenshots-generator)
    - Estructura JS por módulos
    
    ### Added
    - Botón instalar PWA usando https://github.com/khmyznikov/pwa-install
    - Alerta ante perdida de conexión internet
    - Ocultar texto botones overlay acorde a tamaño div general para evitar overflow en tamaños pequeños
    - Botones para carga canales predeterminados
    - Efecto de sonido a botones de carga canales predeterminados [CRT turn on notification por Coolshows101sound](https://freesound.org/people/Coolshows101sound/sounds/709461/)
    - Iconos a botones de quitar todo canal activo, item navbar créditos y descargo de responsabilidad
    - Habilidad de mover botones flotantes
    - Habilidad ocultar individualmente botones de overlay canales
    - Observer para casos donde solo existan 2 o 1 canal activo se ajuste tamaño columna acorde para abarcar todo el width
    
    ### Removed
    - archivo js/json con canales (se creo https://github.com/Alplox/json-teles para hacer fetch, dejando como respaldo ultimo archivo que existió en repositorio)
    
    ### Fixed
    - Opción de entrar a pantalla completa (bug https://github.com/Alplox/teles/issues/1 de si se ingresa con icono ampliar desde algún navegador permanece, pero problema no es único a sitio por lo que se deja ya que globalmente (por lo que vi) no tiene solución) con tecla F11 funcionamiento acorde al esperado.
    
    porfa que no se rompa algo como siempre...
    works on my machine

--- assets/js/botonResetLocalStorage.js
+++ assets/js/botonResetLocalStorage.js
@@ -1,13 +1,13 @@
+import { detectarSiExistenCanalesParaQuitar } from './botonesQuitarTodo.js'
+
 // ----- alerta borrado localstorage
 const btnReset = document.querySelector('#btn-reset');
 const alertReset = document.querySelector('#alert-reset');
 const audioLoopEstaticaCRT = new Audio('assets/sounds/DefectLineTransformer-por-blaukreuz.wav');
 
-btnReset.addEventListener('click', () => {  
+btnReset.addEventListener('click', () => {
   detectarSiExistenCanalesParaQuitar();
   localStorage.clear();
-  audioLimpiarCanalesActivos.play();
-  audioLimpiarCanalesActivos.volume = 0.8;
   audioLoopEstaticaCRT.volume = 0.8;
   audioLoopEstaticaCRT.loop = true;
   audioLoopEstaticaCRT.play();
